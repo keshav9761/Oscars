881a55f07ded48fbdd377699a3fe1264fea3c70f
keshav9761 <keshavtomar53@gmail.com>
2024-08-06 04:31:05 -0400

    fixed 46

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
     res.statusCode(200).json({ msg: "Hello, World!"});
 })
 
-app.get("/one", (req, res) => {
+app.get("/1", (req, res) => {
     
     res.statusCode(200).json({ msg: "Hi"});
 })
