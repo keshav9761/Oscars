1047bfdfa36b71c94ffc0cf6bb1a0dd8d7b0b3b9
keshav9761 <keshavtomar53@gmail.com>
2024-08-07 08:19:40 -0400

    Test get api

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const PORT = 80;
 app.use(cors());
 app.use(Express.static(path.join(__dirname, 'frontend')))
 
+app.get("/api", (req, res) => {
+    res.send({a: 1, b: 2});
+})
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 })
 
-app.get("/api", (req, res) => {
-    res.send({a: 1, b: 2});
-})
 
 app.listen(PORT, () => {
     console.log("Server Started On Post", PORT)
